cf70fca75818520cd28de6f2530e9d194468b41c
Woodberryy <100994363+Woodberryy@users.noreply.github.com>
2022-08-03 12:29:10 +0100

    initial commit

---
+++ game.js
@@ -0,0 +1,28 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+
+function jump() {
+  if (character.classlist != "animate") {
+    character.classList.add("animate");
+  }
+ 
+  setTimeout(function () {
+    character.classList.remove("animate");
+  }, 500);
+}
+var check = setInterval( function() {
+    var charactertop = parseInt(
+      window.getComputedStyle(character).getPropertyValue("top")
+    );
+    var blockleft = parseInt(
+      window.getComputedStyle(block).getPropertyValue("left")
+    );
+    if(blockleft <20 && blockleft >0 && charactertop>=130){
+       
+     
+      block.style.animation="none";
+      block.style.display="none";
+      alert("you lose");
+    }
+  }, 60);
+  
